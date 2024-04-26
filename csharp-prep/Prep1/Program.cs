8f73430d51576a51d5993bddac898ce950e56f9e
DragonLord <olivecollin2451@gmail.com>
2024-04-26 06:59:09 -0600

    Done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+        string firstName = Console.ReadLine(); // Read first name
+
+        Console.Write("Please enter your last name: ");
+        string lastName = Console.ReadLine(); // Read last name
+
+        Console.WriteLine($"Your name is {lastName}, {firstName}, {lastName}."); // Display formatted message
     }
 }
