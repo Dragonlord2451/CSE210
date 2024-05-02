dc2926624de8a019bbb460618da1a4442ab71156
DragonLord <olivecollin2451@gmail.com>
2024-05-01 22:36:18 -0600

    Done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
-class Program
+class GuessMyNumber
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+        int magicNumber = random.Next(1, 101); // Random number between 1 and 100
+        int guess = -1;
+
+        Console.WriteLine("Welcome to Guess My Number!");
+        
+        // Loop until the guess is correct
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
