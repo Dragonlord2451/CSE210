14065304fd1c13f8ff1d1cdfb966456b606638cd
DragonLord <olivecollin2451@gmail.com>
2024-05-08 22:46:04 -0600

    DONE

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Safe Torrent";
+        job1._jobTitle = "Safe manager";
         job1._company = "Walmart";
         job1._startYear = 2002;
         job1._endYear = 2018;
