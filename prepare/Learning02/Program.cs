8b19b07f5f6074e2e18039eadf59c29800a947d2
DragonLord <olivecollin2451@gmail.com>
2024-05-08 22:41:07 -0600

    DONE

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Safe Torrent";
+        job1._company = "Walmart";
+        job1._startYear = 2002;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Team Manager";
+        job2._company = "Walmart";
+        job2._startYear = 2017;
+        job2._endYear = 2021;
+
+        Resume myResume = new Resume();
+        myResume._name = "Jallen Steven";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
